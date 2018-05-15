cbcf51a374aac415054052579e6952c6f0a4c5c0
Patrick Fuller <patrickfuller@gmail.com>
2018-05-15 16:23:51 -0500

    Add set-setpoint command-line capability

--- huber/__init__.py
+++ huber/__init__.py
@@ -11,9 +11,13 @@ def command_line():
     parser = argparse.ArgumentParser(description="Control a Huber bath "
                                      "from the command line.")
     parser.add_argument('ip', help="The bath IP address.")
+    parser.add_argument('--set-setpoint', '-s', default=None, type=float,
+                        help="Sets the bath temperature setpoint.")
     args = parser.parse_args()
 
     async def print_state():
+        if args.set_setpoint is not None:
+            await bath.set_setpoint(args.set_setpoint)
         print(json.dumps(await bath.get(), indent=4, sort_keys=True))
 
     bath = Bath(args.ip)
