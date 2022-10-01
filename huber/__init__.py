4f29d5c6c6cafb28efef2f2938ab8746579b5e0d
Alex Ruddick <a.ruddick@numat-tech.com>
2022-09-30 21:33:41 -0500

    Fix mocks and use a context manager

--- huber/__init__.py
+++ huber/__init__.py
@@ -12,21 +12,19 @@ def command_line(args=None):
     parser.add_argument('ip', help="The bath IP address.")
     parser.add_argument('--set-setpoint', '-s', default=None, type=float,
                         help="Sets the bath temperature setpoint.")
-    args = parser.parse_args()
+    args = parser.parse_args(args)
 
     async def print_state():
         if args.set_setpoint is not None:
             await bath.set_setpoint(args.set_setpoint)
         print(json.dumps(await bath.get(), indent=4, sort_keys=True))
 
-    bath = Bath(args.ip)
     ioloop = asyncio.get_event_loop()
     try:
-        ioloop.run_until_complete(print_state())
+        with Bath(args.ip) as bath:
+            ioloop.run_until_complete(print_state())
     except KeyboardInterrupt:
         pass
-    bath.close()
-    ioloop.close()
 
 
 if __name__ == '__main__':
