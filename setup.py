80f1eab6e111552e8cc0d806e2ed6f1b175ad8a5
Alex Ruddick <a.ruddick@numat-tech.com>
2023-03-06 17:03:04 -0600

    Bump version to 0.5.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as in_file:
 
 setup(
     name='huber',
-    version='0.5.1',
+    version='0.5.2',
     description='Python driver for Huber recirculating baths.',
     long_description=long_description,
     long_description_content_type='text/markdown',
